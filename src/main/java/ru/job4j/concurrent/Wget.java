4356c6215d56c092846ad79fed14befa241124be
Arhip <grfoger@gmail.com>
2022-03-28 17:17:10 +0300

    1.2. Режим ожидания. [#231217 #281353] -corrected

--- src/main/java/ru/job4j/concurrent/Wget.java
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -4,8 +4,7 @@ public class Wget {
     public static void main(String[] args) {
         Thread thread = new Thread(() -> {
             try {
-                int index = 0;
-                for (; index <= 100; index++) {
+                for (int index = 0; index <= 100; index++) {
                     System.out.print("\rLoading : " + index  + "%");
                     Thread.sleep(100);
                 }
