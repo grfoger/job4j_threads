a018a51afcf6ca3960f433a1216f90878eeaec7b
grfoger <grfoger@gmail.com>
2022-03-30 16:53:51 +0300

    1. Visibility. Общий ресурс вне критической секции [#1102 #282159]

--- src/main/java/ru/job4j/io/ParseFileLoader.java
+++ src/main/java/ru/job4j/io/ParseFileLoader.java
@@ -12,16 +12,16 @@ public final class ParseFileLoader {
     }
 
     private String content(Predicate<Character> filter) {
-        String output = "";
+        StringBuilder output = new StringBuilder();
         try (BufferedInputStream in = new BufferedInputStream(new FileInputStream(file))) {
             int data;
             while ((data = in.read()) > 0) {
-                output += filter.test((char) data);
+                output.append(filter.test((char) data));
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
-        return output;
+        return output.toString();
     }
 
     public String getContent() {
