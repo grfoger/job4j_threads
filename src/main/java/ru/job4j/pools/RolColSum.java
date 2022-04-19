8e69f81acc9629a0e7e102daffd0332f9134c114
grfoger <grfoger@gmail.com>
2022-04-19 14:59:55 +0300

    4. CompletableFuture [#361626] init

---
+++ src/main/java/ru/job4j/pools/RolColSum.java
@@ -0,0 +1,75 @@
+package ru.job4j.pools;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+public class RolColSum {
+
+    public static Sums[] sum(int[][] matrix) {
+        Sums[] sums = new Sums[matrix.length];
+        for (int i = 0; i < matrix.length; i++) {
+            if (matrix[i].length != matrix.length) {
+                throw new IllegalArgumentException("Переданная матрица не является квадратной.");
+            }
+            int rowSum = 0;
+            int colSum = 0;
+            for (int j = 0; j < matrix.length; j++) {
+                rowSum += matrix[i][j];
+                colSum += matrix[j][i];
+            }
+            sums[i] = new Sums(rowSum, colSum);
+        }
+        return sums;
+    }
+
+    public static Sums[] asyncSum(int[][] matrix) throws ExecutionException, InterruptedException {
+        Sums[] sums = new Sums[matrix.length];
+        for (int i = 0; i < matrix.length; i++) {
+            if (matrix[i].length != matrix.length) {
+                throw new IllegalArgumentException("Переданная матрица не является квадратной.");
+            }
+            sums[i] = getTask(matrix, i).get();
+        }
+        return sums;
+    }
+
+    public static CompletableFuture<Sums> getTask(int[][] matrix, int row) {
+        return CompletableFuture.supplyAsync(() -> {
+            int rowSum = 0;
+            int colSum = 0;
+            for (int j = 0; j < matrix.length; j++) {
+                rowSum += matrix[row][j];
+                colSum += matrix[j][row];
+            }
+            return new Sums(rowSum, colSum);
+        });
+    }
+
+
+
+    public static class Sums {
+        private int rowSum;
+        private int colSum;
+
+        public Sums(int rowSum, int colSum) {
+            this.rowSum = rowSum;
+            this.colSum = colSum;
+        }
+
+        public int getRowSum() {
+            return rowSum;
+        }
+
+        public void setRowSum(int rowSum) {
+            this.rowSum = rowSum;
+        }
+
+        public int getColSum() {
+            return colSum;
+        }
+
+        public void setColSum(int colSum) {
+            this.colSum = colSum;
+        }
+    }
+}
